a096dd248a6005074b6806f434fee13dcc2ddde2
Konstantin Aleksandrov <konstantin.aleksandrov@kodix.ru>
2017-09-26 00:21:05 +0300

    Layouts

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <h1>Where to output post</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,7 @@
 <?php
 
+Route::get('/posts', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
+
 Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
