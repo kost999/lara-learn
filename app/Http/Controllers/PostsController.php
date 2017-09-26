885356acadd6430f9b011b3d6ebc928a0977e358
Konstantin Aleksandrov <konstantin.aleksandrov@kodix.ru>
2017-09-26 12:21:59 +0300

    Forms

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function index()
@@ -12,5 +14,33 @@ class PostsController extends Controller
 	public function show()
 	{
 		return view('posts.show');
+	}
+
+	public function create()
+	{
+		return view('posts.create');
+	}
+
+	public function store()
+	{
+		// Create a new post
+
+//		$post = new Post();
+
+//		$post->title = request('title');
+//		$post->body = request('body');
+
+		// Save it to the database
+
+//		$post->save();
+
+		Post::create([
+			'title' => request('title'),
+			'body' => request('body'),
+		]);
+
+		// And then redirect to the homepage
+
+		return redirect('/posts');
 	}
 }
