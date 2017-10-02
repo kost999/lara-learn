9afc9d341428e5f41437111347a4d2bce8be48ee
Konstantin <konstantin.alexandrov@kodix.ru>
2017-10-02 09:30:01 +0300

    comments

--- app/Post.php
+++ app/Post.php
@@ -4,5 +4,18 @@ namespace App;
 
 class Post extends Model
 {
+	public function comments()
+	{
+		return $this->hasMany(Comment::class);
+	}
 
+	public function addComment($body)
+	{
+		$this->comments()->create(compact('body'));
+
+//		Comment::create([
+//			'body' => $body,
+//			'post_id' => $this->id,
+//		]);
+	}
 }
