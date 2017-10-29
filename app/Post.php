f41055015271ea2b27293b71b765d50fd3cb426d
Konstantin <konstantin.alexandrov@kodix.ru>
2017-10-29 22:22:54 +0300

    archives

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
 class Post extends Model
 {
 	public function comments()
@@ -25,5 +27,16 @@ class Post extends Model
 	public function user()
 	{
 		return $this->belongsTo(User::class);
+	}
+
+	public function scopeFilter($query, $filters)
+	{
+		if ($month = $filters['month']) {
+			$query->whereMonth('created_at', Carbon::parse($month)->month);
+		}
+
+		if ($year = $filters['year']) {
+			$query->whereYear('created_at', $year);
+		}
 	}
 }
