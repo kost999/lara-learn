fa3d47d6dec3efd8ec152f304eff49b9ce96b357
Konstantin <konstantin.alexandrov@kodix.ru>
2017-12-10 13:33:45 +0300

    view composers

--- app/Post.php
+++ app/Post.php
@@ -39,4 +39,17 @@ class Post extends Model
 			$query->whereYear('created_at', $year);
 		}
 	}
+
+	/**
+	 * Returns posts archives
+	 * @return array
+	 */
+	public static function archives()
+	{
+		return static::selectRaw('year(created_at) as year,monthname(created_at) as month,count(*) published')
+			->groupBy('year', 'month')
+			->orderByRaw('min(created_at) desc')
+			->get()
+			->toArray();
+	}
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema,
+	Illuminate\Support\ServiceProvider,
+	App\Post;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        view()->composer('layouts.sidebar', function($view){
+        	$view->with('archives', Post::archives());
+		});
     }
 
     /**
