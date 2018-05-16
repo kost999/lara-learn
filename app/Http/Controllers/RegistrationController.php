af96983070b60de55474a782c5d8a9b847b772ad
kost999 <ka@kodix.ru>
2018-05-16 19:05:20 +0300

    request classes

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\Welcome;
-use App\User;
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
 {
@@ -12,27 +11,8 @@ class RegistrationController extends Controller
 		return view('registration.create');
 	}
 
-	public function store()
+	public function store(RegistrationForm $request)
 	{
-		// validate form
-		// create and save the user
-		// sign them in
-		// redirect to home page
-
-		$this->validate(request(), [
-			'name' => 'required',
-			'email' => 'required|email',
-			'password' => 'required|confirmed',
-		]);
-
-		$user = User::create(
-			request(['name', 'email', 'password'])
-		);
-
-		auth()->login($user);
-
-		\Mail::to($user)->send(new Welcome($user));
-
 		return redirect()->home();
 	}
 }
